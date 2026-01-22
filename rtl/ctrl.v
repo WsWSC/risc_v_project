a896c76a8e8ca5bb91b6ae75596b44f3d8ae74bc
WsWSC <wsc19970805@gmail.com>
2026-01-22 21:47:46 +0800

    add ctrl.v

---
+++ rtl/ctrl.v
@@ -0,0 +1,14 @@
+module ctrl(
+    input   wire        jump_flag_i ,
+    input   wire        hold_flag_i ,
+    input   wire[31:0]  jump_addr_i ,
+
+    output  wire        hold_flag_o ,
+    output  wire        jump_flag_o ,
+    output  wire[31:0]  jump_addr_o ,
+    output  wire        jump_en     
+);
+
+
+
+endmodule
