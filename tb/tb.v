876848f9196dad76cd043542808017e94d356775
WsWSC <wsc19970805@gmail.com>
2026-01-26 22:27:00 +0800

    pass add/addi testbench in tb/inst_txt

--- tb/tb.v
+++ tb/tb.v
@@ -5,7 +5,7 @@
 //  License : Personal / Educational Use
 ////////////////////////////////////////////////////////////
 
-//`timescale 1ns/1ps
+`timescale 1ns/1ps
 
 module tb;
 
@@ -24,19 +24,47 @@ module tb;
 
     // rom default val
     initial begin
-        $readmemb("../tb/test_input.txt", tb.soc_inst.rom_inst.rom_mem);
+        $readmemh("../tb/inst_txt/rv32ui-p-addi.txt", tb.soc_inst.rom_inst.rom_mem);
     end
 
+    //initial begin
+    //    while (1) begin
+    //        @(posedge clk)
+    //        $display("=================\n");
+    //        $display("x3  reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[3] );
+    //        $display("x27 reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[27]);
+    //        $display("x28 reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[28]);
+    //        #10;
+    //    end
+    //end
+
+    wire [31:0] x3  = tb.soc_inst.core_inst.regs_inst.regs[3]  ;
+    wire [31:0] x26 = tb.soc_inst.core_inst.regs_inst.regs[26] ;
+    wire [31:0] x27 = tb.soc_inst.core_inst.regs_inst.regs[27] ;
+
+    integer i;
+
     initial begin
-        while (1) begin
-            @(posedge clk)
-            $display("=================\n");
-            $display("x27 reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[27]);
-            $display("x28 reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[28]);
-            $display("x29 reg val is %d\n", tb.soc_inst.core_inst.regs_inst.regs[29]);
-            #10;
+        wait(x26 == 32'b1);
+        repeat(2) @(posedge clk);
+
+        if(x27 == 32'b1) begin
+            $display("##################################\n");
+            $display("##########     pass     ##########\n");
+            $display("##################################\n");
+
+        end else begin
+            $display("##################################\n");
+            $display("##########     fail     ##########\n");
+            $display("##################################\n");
+
+            $display("fail at test case %2d\n", x3);
+            for (i = 0; i < 32; i = i + 1) begin
+                $display("x%2d reg val is %d\n", i, tb.soc_inst.core_inst.regs_inst.regs[i]);
+            end
         end
-    end
+
+	end
 
     soc soc_inst(
         .clk        (clk),
